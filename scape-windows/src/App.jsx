539e3d2ea164ddbd0b6c4fefd2c154bc21a39141
joel santos <santoaprenderdb@gmail.com>
2025-04-11 19:50:09 +0200

    juego parte 1

--- scape-windows/src/App.jsx
+++ scape-windows/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// function App() {
   // const [count, setCount] = useState(0)
 
   // return (
@@ -30,11 +30,34 @@ function App() {
   //     </p>
   //   </>
   // );
+// }
 
-  //juego de prueba tic tac toc
-    return(
-      <h1>hola h1</h1>
-    );
+//juego de prueba tic tac toc
+const TURNS = {
+  X: "x",
+  O: "o"
+};
+const board = Array(9).fill(null);
+
+function App(){
+  return (
+      <main className='board'>
+        <h1>Tic Tac Toc</h1>
+        <select className='game'>
+          {
+            board.map((_, index) => {
+              return (
+                <div className='cell' key={index}>
+                  <span className='cell_content'>
+                    {index}
+                  </span>
+                </div>
+              );
+            })
+          }
+        </select>
+      </main>
+  );
 }
 
-export default App;
+  export default App;
