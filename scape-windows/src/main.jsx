52e35513b9d0c3f5397a0e9b99ec03b91c397e19
joel santos <santoaprenderdb@gmail.com>
2025-04-11 19:01:53 +0200

    ajustes iniciales juego tic tac toc

--- scape-windows/src/main.jsx
+++ scape-windows/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css' //styles
+import './index.css' //styles
 
 //import desde su archivo de origen
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
   </StrictMode>
 );
